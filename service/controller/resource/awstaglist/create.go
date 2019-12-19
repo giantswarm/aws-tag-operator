ef741ba1e76bd1896653a97abcb61ed8de4eb651
pipo02mix <fernando@giantswarm.io>
2019-12-19 15:27:41 +0100

    List all PVs of the cluster

--- service/controller/resource/awstaglist/create.go
+++ service/controller/resource/awstaglist/create.go
@@ -4,30 +4,37 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/giantswarm/microerror"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func (r *Resource) EnsureCreated(ctx context.Context, obj interface{}) error {
-	fmt.Println("YOLO")
 	al, err := r.toAWSTagList(obj)
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
 	for _, tag := range al.Spec.TagCollection {
-		fmt.Println(tag)
+		fmt.Printf("Key: %s, Value: %s", tag.Key, tag.Value)
 	}
 
-	i := &ec2.DescribeVolumesInput{}
-	o, err := r.awsClients.EC2Client().DescribeVolumes(i)
+	pvList, err := r.k8sClient.K8sClient().CoreV1().PersistentVolumes().List(metav1.ListOptions{})
 	if err != nil {
 		return microerror.Mask(err)
 	}
 
-	for _, v := range o.Volumes {
-		fmt.Println(v.GoString())
+	for _, pv := range pvList.Items {
+		fmt.Println(pv.Spec.AWSElasticBlockStore.VolumeID)
 	}
+	// i := &ec2.DescribeVolumesInput{}
+	// o, err := r.awsClients.EC2Client().DescribeVolumes(i)
+	// if err != nil {
+	// 	return microerror.Mask(err)
+	// }
+
+	// for _, v := range o.Volumes {
+	// 	fmt.Println(v.GoString())
+	// }
 
 	return nil
 }
